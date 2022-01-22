7704dddef46a517c59d133fcdc651bc375424d2a
Maria Petrova <maria.petrova239@bk.ru>
2022-01-22 19:18:00 +0300

    more plays

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-alert( 'Привет, мир!' );
+// "use strict"
+
+/*alert('Привет'); 
+alert('Мир');
+
+let message = "Теперь всё в порядке";
+alert(message);
+
+[1, 2].forEach(alert)
+
+num = 5;
+
+alert(num);
+
+let test = 1;
+test = "test text";
+alert(typeof(test));
+
+let testUndef;
+alert(typeof(testUndef));*/
+
+let result = prompt("title", ["default"]);
+alert(result);
